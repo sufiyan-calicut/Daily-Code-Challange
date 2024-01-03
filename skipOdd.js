33a2a89675521d42fe4cfe9f212f653514c67619
sufiyan-calicut <sufiyanemcalicut@gmail.com>
2024-01-03 19:08:42 +0530

    update on skipOdd

--- skipOdd.js
+++ skipOdd.js
@@ -1,10 +1,16 @@
 // skip to elements if the element is odd
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
-  if (array[i] % 2 == 1) {
-    i = i + 2;
+function skipOdd(array) {
+  let newAr = [];
+  for (let i = 0; i < array.length; i++) {
+    let value = array.splice(i, 1);
+    newAr.push(value[0]);
+    // console.log(array[i]);
+    if (array[i] % 2 == 1) {
+      i = i + 2;
+    }
   }
+  return newAr;
 }
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(skipOdd(array));
