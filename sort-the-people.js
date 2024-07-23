4a5dbd18b80eac8784ceda436cbdf0fb9730abb3
sufiyan-calicut <sufiyanemcalicut@gmail.com>
2024-07-23 20:40:19 +0530

    Sort the people

---
+++ sort-the-people.js
@@ -0,0 +1,38 @@
+/**
+2418. Sort the People
+Easy
+Topics
+Companies
+Hint
+You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+
+ 
+
+Example 1:
+
+Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+Output: ["Mary","Emma","John"]
+Explanation: Mary is the tallest, followed by Emma and John.
+Example 2:
+
+Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+Output: ["Bob","Alice","Bob"]
+Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+ 
+ */
+
+var sortPeople = function (names, heights) {
+const people = names.map((name,index) => ({name, height:heights[index]}));
+people.sort((a,b) => b.height - a.height);
+
+return people.map((person) => person.name)
+    
+};
+const names = ['Mary', 'John', 'Emma'];
+const heights = [180, 165, 170];
+
+console.log(sortPeople(names, heights));
